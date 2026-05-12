10c441856f2cfa38fb41f806be6fc3f1af293c90
ThomasCifuentes <thomas.cifuentes@udla.edu.ec>
2026-05-12 11:47:27 -0500

    cambio de valores dentro del arreglo

--- main2.c
+++ main2.c
@@ -3,8 +3,8 @@ int main()
 {
     char vocales[] = {'a','e','i','o','u',};
     char *ptr;
-
     ptr = &vocales[0];
+    *(ptr+3)='x';
     for (int i=0; i< 5; i++)
     {
         printf("%c",vocales[i]);
